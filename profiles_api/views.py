43cf5f6d0d7c127049ab25830d5e67ed537fce82
prathap442 <pmohansaikrishna@gmail.com>
2020-05-08 16:02:41 +0530

    added: search functionality for the Userprofiles

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response 
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 
 from profiles_api import serializers
 from rest_framework import viewsets
@@ -101,3 +102,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     permission_classes = (permissions.UpdateOwnProfile,)
     authentication_classes = (TokenAuthentication,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
